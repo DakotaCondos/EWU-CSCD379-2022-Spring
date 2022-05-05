d0fa7bb1e8b9557b276a4a0dbb7126824b83d1df
DakotaCondos <dakota242424@live.com>
2022-05-05 14:41:07 -0700

    updated ScoreStatsServiceTests

--- Wordle.Api.Tests/ScoreStatsServiceTests.cs
+++ Wordle.Api.Tests/ScoreStatsServiceTests.cs
@@ -14,7 +14,7 @@ namespace Wordle.Api.Tests
     public class ScoreStatsServiceTests
     {
         private AppDbContext _context;
-        
+
         public ScoreStatsServiceTests()
         {
             var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
@@ -22,7 +22,7 @@ namespace Wordle.Api.Tests
             _context = new AppDbContext(contextOptions.Options);
             _context.Database.Migrate();
             ScoreStatsService.Seed(_context);
-            
+
         }
         [TestMethod]
         public void GetScoreStats()
@@ -36,12 +36,17 @@ namespace Wordle.Api.Tests
         public void CalculateAverageSeconds()
         {
             ScoreStatsService sut = new ScoreStatsService(_context);
-            sut.Update(1,2);
-            Assert.AreEqual(1, sut.GetScoreStats().First(f => f.Score==1).TotalGames);
-            Assert.AreEqual(2, sut.GetScoreStats().First(f => f.Score == 1).AverageSeconds);
+            ScoreStat scoreStat1 = sut.GetScoreStats().First(f => f.Score == 1).Clone();
+
+            sut.Update(1, 2);
+            Assert.AreEqual((scoreStat1.TotalGames + 1), sut.GetScoreStats().First(f => f.Score == 1).TotalGames);
+            Assert.AreEqual(scoreStat1.AverageSeconds + (scoreStat1.AverageSeconds - 2) / (scoreStat1.TotalGames + 1), sut.GetScoreStats().First(f => f.Score == 1).AverageSeconds);
+
+            ScoreStat scoreStat2 = sut.GetScoreStats().First(f => f.Score == 1).Clone();
             sut.Update(1, 4);
-            Assert.AreEqual(2, sut.GetScoreStats().First(f => f.Score == 1).TotalGames);
-            Assert.AreEqual(3, sut.GetScoreStats().First(f => f.Score == 1).AverageSeconds);
+
+            Assert.AreEqual((scoreStat2.TotalGames + 1), sut.GetScoreStats().First(f => f.Score == 1).TotalGames);
+            Assert.AreEqual(scoreStat2.AverageSeconds + (scoreStat2.AverageSeconds - 4) / (scoreStat2.TotalGames + 1), sut.GetScoreStats().First(f => f.Score == 1).AverageSeconds);
         }
     }
 }
